0754fbc82ae7b43f1529d57a4fe5b2a8a6c73513
Angela Trp <angela.trpkovska@symphony.is>
2020-10-25 19:13:46 +0100

    Add session and speaker model, database and api controllers

---
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SpeakersController.java
@@ -0,0 +1,31 @@
+package com.pluralsight.conferencedemo.controllers;
+
+import com.pluralsight.conferencedemo.models.Speaker;
+import com.pluralsight.conferencedemo.repositories.SpeakerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/speakers")
+public class SpeakersController {
+
+    @Autowired
+    private SpeakerRepository speakerRepository;
+
+    @GetMapping("/list")
+    public List<Speaker> getSpeakers(){
+        return speakerRepository.findAll();
+    }
+
+    @GetMapping("/find/{id}")
+    public Speaker getSpeaker(@PathVariable Long id){
+        return speakerRepository.getOne(id);
+    }
+
+
+}
